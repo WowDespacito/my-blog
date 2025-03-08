2934f81f1f9601bd1da7995ba1d39c2c5ad35a5a
wowDespacito <wangzhi_wow@163.com>
2025-03-09 01:00:37 +0800

    http配置完成，下一步配置https

--- src/main/java/com/wowdespacito/my_blog/controller/Admin/AdminBlogControl.java
+++ src/main/java/com/wowdespacito/my_blog/controller/Admin/AdminBlogControl.java
@@ -1,7 +1,6 @@
 package com.wowdespacito.my_blog.controller.Admin;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wowdespacito.my_blog.pojo.Blog;
@@ -28,23 +27,26 @@ public class AdminBlogControl {
     private BlogService blogService;
 
     @PostMapping("/addBlog")
-    public Response<String> add(@RequestBody @Validated Blog blog) {
-        logger.info("新增blog：",blog.getTitle());
+    public Response<String> add(@RequestBody Blog blog) {
+        logger.info("新增blog：" + blog.getTitle());
         blogService.addBlog(blog);
+        logger.info("新增blog成功" + blog.getTitle());
         return Response.success();
     }
 
     @PutMapping("/updateBlog")
     public Response<String> update(@RequestBody @Validated(Blog.Update.class) Blog blog) {
-        logger.info("更新blog：",blog.getId());
+        logger.info("更新blog：" + blog.getId().toString());
         blogService.updateBlog(blog);
+        logger.info("更新blog成功");
         return Response.success();
     }
-    
+
     @DeleteMapping("/deleteBlog/{id}")
     public Response<String> delete(@PathVariable Integer id) {
-        logger.info("移除blog：id="+ id);
+        logger.info("移除blog：id=" + id.toString());
         blogService.deleteBlog(id);
+        logger.info("移除blog成功");
         return Response.success();
     }
 }
